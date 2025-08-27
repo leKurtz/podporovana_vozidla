65ab941a53916d887d895ce27e884d4231a44ee1
leKurtz <vysusil.theodor@gmail.com>
2025-08-27 14:35:31 +0200

    Add files via upload

--- hw_podpora_app.py
+++ hw_podpora_app.py
@@ -18,7 +18,7 @@ st.title("Kontrola podpory HW pro vozidla")
 
 # Formulář pro zadání vstupů
 with st.form("hledani_form"):
-    hw = st.text_input("Zadej typ HW (818, 820, Teltonika 150)").strip()
+    hw = st.text_input("Zadej typ HW (820, FMX003, FMX150)").strip()
     vyrobce = st.text_input("Zadej výrobce vozidla").strip()
     model = st.text_input("Zadej model vozidla").strip()
     submitted = st.form_submit_button("Hledat")
@@ -36,6 +36,8 @@ if submitted:
 
             for _, radek in filtrovano.iterrows():
                 st.write("---")
+                if 'Model_+' in df.columns and pd.notna(radek.get('Model_+')):
+                    st.write(f"**Model upřesnění:** {radek['Model_+']}")
                 if 'Rok' in df.columns and pd.notna(radek.get('Rok')):
                     st.write(f"**Rok:** {radek['Rok']}")
                 if 'CMD' in df.columns and pd.notna(radek.get('CMD')):
@@ -44,6 +46,12 @@ if submitted:
                     st.write(f"**NKP příkaz:** {radek['NKP']}")
                 if 'Zapojeni' in df.columns and pd.notna(radek.get('Zapojeni')):
                     st.write(f"**Zapojení:** {radek['Zapojeni']}")
+                if 'Tachometr' in df.columns and pd.notna(radek.get('Tachometr')):
+                    st.write(f"**Tachometr:** {radek['Tachometr']}")
+                if 'Nadrz litry' in df.columns and pd.notna(radek.get('Nadrz litry')):
+                    st.write(f"**Nádrž litry:** {radek['Nadrz litry']}")
+                if 'Nadrz %' in df.columns and pd.notna(radek.get('Nadrz %')):
+                    st.write(f"**Nádrž %:** {radek['Nadrz %']}")
         else:
             st.error("Ne, zadaná kombinace HW, výrobce a modelu nebyla nalezena.")
     else:
